5703116505b2d63274ed9fb514536e45da94d444
maharab12 <120368472+maharab12@users.noreply.github.com>
2023-11-22 22:21:46 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,146 @@
 import 'package:flutter/material.dart';
-import 'package:introduction_screen/introduction_screen.dart';
-import 'home.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
+class AnotherPage extends StatefulWidget {
+  final String title;
+  AnotherPage(this.title);
+
+  @override
+  State<AnotherPage> createState() => _AnotherPageState();
+}
+
+class _AnotherPageState extends State<AnotherPage> {
+  final videourl="https://youtu.be/YMx8Bbev6T4";
+  late YoutubePlayerController _controller;
+  @override
+  void initState() {
+    final videoId=YoutubePlayer.convertUrlToId(videourl);
+    _controller= YoutubePlayerController(initialVideoId: videoId??'');
+    flags:YoutubePlayerFlags(
+      autoPlay: false
+    );
+    super.initState();
+  }
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'anotherpage.dart';
+
 void main(){
   runApp(Myapp());
 }
-
 class Myapp extends StatelessWidget {
   const Myapp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home:Splash() ,
+      home: Homepage(),
+    );
+  }
+}
+class Homepage extends StatefulWidget {
+  const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        body: Column(
+
+          children: [
+            Container(
+              height: 130,
+              decoration: BoxDecoration(
+                  color: Colors.red,
+                borderRadius: BorderRadius.only(bottomLeft:Radius.circular(35),bottomRight:Radius.circular(35
+              ),)
+              ),child: Center(
+                child: ListTile(
+                title: Text("@Hasan Islam"),
+                  subtitle: Text("Professional .....",style: TextStyle(fontSize: 20),),
+                  trailing: CircleAvatar(
+                    radius: 25,
+                    backgroundImage: Image.network("https://i.pinimg.com/736x/17/5a/7e/175a7e5aa143451d4029b3bdc4714891.jpg").image,
+                  )),
+              ),),
+            Expanded(child: Container(
+              child: GridView.count(crossAxisCount: 2,
+              mainAxisSpacing: 5,
+                crossAxisSpacing: 5,
+                children: [
+                  item(context, "Vidoes",Icon(Icons.video_collection)),
+                  item(context, "upload flie", Icon(Icons.upload)),
+                  item(context,"help", Icon(Icons.upcoming),),
+                  item(context,"Upload", Icon(Icons.upload))
+
+                ],
+
+              ),
+            ))
+          ],
+        ),
+      ),
     );
   }
 }
 
-class Splash extends StatefulWidget {
-  const Splash({super.key});
+item(BuildContext context, String title,IconData) {
+return GestureDetector(
+  onTap:(){ Navigator.push(context,CupertinoPageRoute(builder: (context)=>AnotherPage(title)));},
+  child:
+    Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        height:300,
+        width: 300,
+        color: Colors.white,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            IconData,
+            Text(title)
+          ],
+        ),
+      ),
+    )
+);
 
-  @override
-  State<Splash> createState() => _SplashState();
 }
-class _SplashState extends State<Splash> {
+
+
+
+
+
   @override
   Widget build(BuildContext context) {
-    return IntroductionScreen(pages: [
-      PageViewModel(title: "First",
-          body: "This is first desctiption",
-          image: Image.network("https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg")
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(widget.title),
       ),
-      PageViewModel(
-          title: "First",
-          body: "This is first desctiption",
-          image:Image.network("https://cdn.pixabay.com/photo/2015/07/09/22/45/tree-838667_1280.jpg")
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          YoutubePlayer(controller: _controller,
+          showVideoProgressIndicator:true,
+          bottomActions: [
+            CurrentPosition(),
+            ProgressBar(
+              isExpanded: true,
 
-      ),
-      PageViewModel(
-          title: "First",
-          body: "This is first desctiption",
-
-          image: Image.network("https://cdn.pixabay.com/photo/2018/04/20/09/49/sky-3335585_1280.jpg")
-      ),
-    ],
-      onDone: (){Navigator.push(context, MaterialPageRoute(builder: (context)=>Homepage()));},
-      next: Icon(Icons.forward),
-
-      showSkipButton: true,
-      skip: Text("Skip"),
-      done: Text("Done"),);
+            ),
+            PlaybackSpeedButton()
+          ],
+          ),
+        ],
+      )
+    );
   }
 }
 
